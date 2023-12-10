2801d2642dc1512b192795a2afb0f4facd46de97
Revaldyhfz <revaldyhafiz16@gmail.com>
2023-12-10 18:12:06 +0700

    Profile 1/2 progress

--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:mybooklistmobile/models/booklistprofile/readingbooks.dart';
 
 void main() {
   runApp(MyApp());
@@ -51,57 +54,111 @@ class BookListScreen extends StatelessWidget {
   }
 }
 
-class BookCategorySection extends StatelessWidget {
+class BookCategorySection extends StatefulWidget {
   final String title;
 
   BookCategorySection({required this.title});
 
   @override
-  Widget build(BuildContext context) {
-    // Dummy data for book images
-    List<String> bookImages = [
-      'path/to/your/book/image1.png',
-      'path/to/your/book/image2.png',
-      'path/to/your/book/image3.png',
-    ];
+  _BookCategorySectionState createState() => _BookCategorySectionState();
+}
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-          child: Text(title, style: Theme.of(context).textTheme.headline5),
-        ),
-        Container(
-          height: 200, // Adjust height accordingly
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: bookImages.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  // Navigate to book page
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => BookPage()),
-                  );
-                },
-                child: Container(
-                  width: 150, // Adjust width accordingly
-                  child: Image.network(bookImages[index], fit: BoxFit.cover), // Use Image.asset for local images
+class _BookCategorySectionState extends State<BookCategorySection> {
+  late Future<List<Product>> books;
+
+  @override
+  void initState() {
+    super.initState();
+    // Obtain the user's authentication token during the login process
+    String authToken = "your_auth_token_here";
+    books = fetchBooks(widget.title, authToken);
+  }
+
+  Future<List<Product>> fetchBooks(String category, String authToken) async {
+    String endpoint;
+
+    switch (category) {
+      case 'Planned to Read':
+        endpoint = 'http://localhost:8000/profile/get-planned/';
+        break;
+      case 'Reading':
+        endpoint = 'http://localhost:8000/profile/get-reading/';
+        break;
+      case 'Completed':
+        endpoint = 'http://localhost:8000/profile/get-completed/';
+        break;
+      default:
+        throw Exception('Invalid category: $category');
+    }
+
+    final response = await http.get(
+      Uri.parse(endpoint),
+      headers: {'Authorization': 'Bearer $authToken'},
+    );
+
+    if (response.statusCode == 200) {
+      return productFromJson(response.body);
+    } else {
+      throw Exception('Failed to load books. Status code: ${response.statusCode}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Product>>(
+      future: books,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          List<Product> bookList = snapshot.data!;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                child: Text(widget.title, style: Theme.of(context).textTheme.headline5),
+              ),
+              Container(
+                height: 200,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: bookList.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => BookPage(bookList[index])),
+                        );
+                      },
+                      child: Container(
+                        width: 150,
+                        child: Image.network(
+                          bookList[index].fields.imageLink,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
-          ),
-        ),
-      ],
+              ),
+            ],
+          );
+        }
+      },
     );
   }
 }
 
-
 class BookPage extends StatelessWidget {
-  // In a real app, you would pass the book details to this page.
+  final Product book;
+
+  BookPage(this.book);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,7 +166,16 @@ class BookPage extends StatelessWidget {
         title: Text('Book Details'),
       ),
       body: Center(
-        child: Text('Details of the book go here'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Title: ${book.fields.title}'),
+            Text('Author: ${book.fields.author}'),
+            Text('Publisher: ${book.fields.publisher}'),
+            Text('Description: ${book.fields.description}'),
+            // Add more details as needed
+          ],
+        ),
       ),
     );
   }
