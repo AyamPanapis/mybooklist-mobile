48fdf621a45dbc6c94e364ab08a346f9c406dba2
Revaldyhfz <revaldyhafiz16@gmail.com>
2023-12-11 14:02:13 +0700

    Profile Page 3/5 done

--- lib/screens/profile/profile_page.dart
+++ lib/screens/profile/profile_page.dart
@@ -2,181 +2,147 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:mybooklistmobile/models/booklistprofile/readingbooks.dart';
+import 'package:mybooklistmobile/widgets/left_drawer.dart';
 
-void main() {
-  runApp(MyApp());
+class ProductPage extends StatefulWidget {
+  const ProductPage({Key? key}) : super(key: key);
+
+  @override
+  _ProductPageState createState() => _ProductPageState();
 }
 
-class MyApp extends StatelessWidget {
+class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Good Reads!',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: BookListScreen(),
-    );
-  }
-}
-
-class BookListScreen extends StatelessWidget {
-  final List<String> categories = ['Planned to Read', 'Reading', 'Completed'];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Good Reads!'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              'Current User',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                return BookCategorySection(
-                  title: categories[index],
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BookCategorySection extends StatefulWidget {
-  final String title;
-
-  BookCategorySection({required this.title});
-
-  @override
-  _BookCategorySectionState createState() => _BookCategorySectionState();
-}
-
-class _BookCategorySectionState extends State<BookCategorySection> {
-  late Future<List<Product>> books;
-
-  @override
-  void initState() {
-    super.initState();
-    // Obtain the user's authentication token during the login process
-    String authToken = "your_auth_token_here";
-    books = fetchBooks(widget.title, authToken);
-  }
-
-  Future<List<Product>> fetchBooks(String category, String authToken) async {
-    String endpoint;
-
-    switch (category) {
-      case 'Planned to Read':
-        endpoint = 'http://localhost:8000/profile/get-planned/';
-        break;
-      case 'Reading':
-        endpoint = 'http://localhost:8000/profile/get-reading/';
-        break;
-      case 'Completed':
-        endpoint = 'http://localhost:8000/profile/get-completed/';
-        break;
-      default:
-        throw Exception('Invalid category: $category');
-    }
-
-    final response = await http.get(
-      Uri.parse(endpoint),
-      headers: {'Authorization': 'Bearer $authToken'},
-    );
-
-    if (response.statusCode == 200) {
-      return productFromJson(response.body);
-    } else {
-      throw Exception('Failed to load books. Status code: ${response.statusCode}');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<List<Product>>(
-      future: books,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
-        } else {
-          List<Product> bookList = snapshot.data!;
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-                child: Text(widget.title, style: Theme.of(context).textTheme.headline5),
-              ),
-              Container(
-                height: 200,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: bookList.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => BookPage(bookList[index])),
-                        );
-                      },
-                      child: Container(
-                        width: 150,
-                        child: Image.network(
-                          bookList[index].fields.imageLink,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('MyBookList'),
+          backgroundColor: const Color(0xFF001C30),
+          foregroundColor: Colors.white,
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Planned'),
+              Tab(text: 'Reading'),
+              Tab(text: 'Completed'),
             ],
-          );
-        }
-      },
-    );
-  }
-}
-
-class BookPage extends StatelessWidget {
-  final Product book;
-
-  BookPage(this.book);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Book Details'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+        drawer: const Drawer(
+          child: LeftDrawer(),
+        ),
+        backgroundColor: const Color(0xFF001C30),
+        body: const TabBarView(
           children: [
-            Text('Title: ${book.fields.title}'),
-            Text('Author: ${book.fields.author}'),
-            Text('Publisher: ${book.fields.publisher}'),
-            Text('Description: ${book.fields.description}'),
-            // Add more details as needed
+            BookCategoryPage(endpoint: 'get-planned'),
+            BookCategoryPage(endpoint: 'get-reading'),
+            BookCategoryPage(endpoint: 'get-completed'),
           ],
         ),
       ),
+    );
+  }
+}
+class BookCategoryPage extends StatelessWidget {
+  final String endpoint;
+
+  const BookCategoryPage({Key? key, required this.endpoint}) : super(key: key);
+
+  Future<List<Product>> fetchProduct() async {
+    try {
+      var url = Uri.parse('http://localhost:8000/profile/$endpoint/');
+      var response = await http.get(
+        url,
+        headers: {"Content-Type": "application/json"},
+      );
+
+      print('Response: ${response.body}'); // Add this line for debugging
+
+      if (response.statusCode == 200) {
+        var data = jsonDecode(utf8.decode(response.bodyBytes));
+        List<Product> list_product = [];
+        for (var d in data) {
+          if (d != null) {
+            list_product.add(Product.fromJson(d));
+          }
+        }
+        return list_product;
+      } else {
+        // Handle non-200 status codes
+        throw 'Failed to load data, status code: ${response.statusCode}';
+      }
+    } catch (error) {
+      // Handle general errors
+      print('Error fetching data: $error');
+      throw 'Failed to load data';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: fetchProduct(),
+      builder: (context, AsyncSnapshot<List<Product>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text(
+              "Error: ${snapshot.error}",
+              style: TextStyle(color: Color(0xFF64CCC5), fontSize: 20),
+            ),
+          );
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(
+            child: Text(
+              "No product data available.",
+              style: TextStyle(color: Color(0xFF64CCC5), fontSize: 20),
+            ),
+          );
+        } else {
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (_, index) => Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 12,
+              ),
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${snapshot.data![index].fields.title}",
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text("${snapshot.data![index].fields.author}"),
+                  const SizedBox(height: 10),
+                  Text("${snapshot.data![index].fields.publisher}"),
+                  const SizedBox(height: 10),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          snapshot.data![index].fields.imageLink,
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
